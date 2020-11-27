aaa9f44bc664d37a9f27f6d13e8a2fb9885bb60b
Xenacode-art <kajaobinna1@gmail.com>
2020-11-26 20:48:13 -0500

    updated my web

--- mysite/mysite/blog/urls.py
+++ mysite/mysite/blog/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path("feed/rss", LatestPostsFeed(), name="post_feed"),
     path("feed/atom", AtomSiteNewsFeed()),
     path("", views.PostList.as_view(), name="home"),
-    # path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    #path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path("<slug:slug>/", views.post_detail, name="post_detail"),
 ]
 
